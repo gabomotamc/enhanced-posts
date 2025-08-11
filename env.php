38040579078795fd4a50ce0e48e3ad9a2f8cdb3d
Gabo Mota Chong <gabomota.me@gmail.com>
2025-08-10 23:45:31 -0300

    make dymanic called styles

--- env.php
+++ env.php
@@ -2,4 +2,5 @@
 define('EPP_VERSION', "1.0.0");
 define('EPP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 define('EPP_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
-define('EPP_PLUGIN_PREFIX', "epp_"); 
+define('EPP_PLUGIN_PREFIX', "epp_");
+define('EPP_PLUGIN_CURRENT_MAIN_STYLE', "main"); 
--- includes/public-scripts.php
+++ includes/public-scripts.php
@@ -2,7 +2,7 @@
 function epp_public_render_posts() {
     wp_enqueue_style(
         'epp-render-posts', 
-        EPP_PLUGIN_URL . 'assets/public/css/main.css', 
+        EPP_PLUGIN_URL . 'assets/public/css/'.EPP_PLUGIN_CURRENT_MAIN_STYLE.'.css', 
         array(), 
         EPP_VERSION, 
         'all'
