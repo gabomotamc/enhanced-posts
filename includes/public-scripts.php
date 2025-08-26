63030d6243b8922ae329edfeefc83b7fe9978343
Gabo Mota Chong <gabomota.me@gmail.com>
2025-08-25 22:25:23 -0300

    improve - styles just for post

--- includes/public-scripts.php
+++ includes/public-scripts.php
@@ -7,5 +7,12 @@ function epp_public_render_posts() {
         EPP_VERSION, 
         'all'
     );
+
+    if ( is_single() ) {
+        wp_enqueue_style( 
+            'epp-render-posts-single-post-style', 
+            EPP_PLUGIN_URL . 'assets/public/css/'.EPP_PLUGIN_CURRENT_POST_STYLE.'.css', 
+        );
+    }    
 }
 add_action( 'wp_enqueue_scripts', 'epp_public_render_posts' );
