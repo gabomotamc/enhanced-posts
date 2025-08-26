0933cc31b637efc20ea866365bed06c06392fe4f
Gabo Mota Chong <gabomota.me@gmail.com>
2025-08-26 01:42:59 +0000

    ignore files

--- env.php
+++ env.php
@@ -3,9 +3,5 @@ define('EPP_VERSION', "1.0.0");
 define('EPP_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 define('EPP_PLUGIN_PATH', plugin_dir_path( __FILE__ ));
 define('EPP_PLUGIN_PREFIX', "epp_");
-<<<<<<< HEAD
-define('EPP_PLUGIN_CURRENT_MAIN_STYLE', "garket-main"); 
-=======
 define('EPP_PLUGIN_CURRENT_MAIN_STYLE', "garket-main");
 define('EPP_PLUGIN_CURRENT_POST_STYLE', "garket-post");
->>>>>>> 338596ad181ce4179a8ddb2302ceb7bb8b2306f6
